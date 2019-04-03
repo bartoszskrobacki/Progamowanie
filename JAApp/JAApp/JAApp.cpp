1a2aabd6e8cf38c11d227ebc54e64878ec1739e4
bartoszskrobacki <bartoszskrobacki@gmail.com>
2019-04-03 19:38:58 +0200

    123

--- JAApp/JAApp/JAApp.cpp
+++ JAApp/JAApp/JAApp.cpp
@@ -2,24 +2,54 @@
 // JAApp.cpp : Defines the entry point for the console application. //
 #include "stdafx.h" 
 #include <windows.h>
-
+#include "JAApp.h"
 //extern "C" int _stdcall MyProc1 (DWORD x, DWORD y);
 //extern "C" int _stdcall MyProc2 (DWORD x, DWORD y);
 typedef void(__stdcall *MyProc2)(int, int);
 
 int _tmain(int argc, _TCHAR* argv[])  { 
-	//MyProc2(2,3);
-	HINSTANCE DynamicLib = LoadLibraryA("JADll");
-	if (!DynamicLib) return 0;
+	char szString[] = {'A','G','I','K','S','Z','J', 0xFF}; // definicja ciagu znakow 
 
-	MyProc2 FunctionPtr = (MyProc2)GetProcAddress(DynamicLib, "MyProc2");
-	
-	if(FunctionPtr) {
-		int x = 0x7FFFFFFF, y = 4;
-		FunctionPtr(x, y);
+	printf("FindChar_1 %s\n", (FindChar_1()==1 ? "Found J" : "Not Found J"));
+	printf("FindChar_2 %s\n", (FindChar_2()==1 ? "Found J" : "Not Found J"));
+	printf("FindChar_3 %s\n", (FindChar_3(szString) ? "Found J" : "Not Found J"));
+	printf("FindChar_4 %s\n", (FindChar_4()==1 ? "Found J" : "Not Found J"));
+	printf("FindChar_5 %s\n", (FindChar_5()==1 ? "Found J" : "Not Found J"));
+	printf("FindChar_6 %s\n", (FindChar_6()==1 ? "Found J" : "Not Found J"));
+
+	//int a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0;
+	//ReadTime_1(&a1, &a2, &a3, &a4, &a5);
+	//printf("Czas przetwarzania: %i\n", a1);
+	for(int i=0; i<4; i++){
+	printf("Czas przetwarzania 1: %i\n", ReadTime_1(FindChar_1));
+	}
+	printf("\n");
+	for(int i=0; i<4; i++){
+	printf("Czas przetwarzania 2: %i\n", ReadTime_1(FindChar_2));
+	}
+	printf("\n");
+	for(int i=0; i<4; i++){
+	printf("Czas przetwarzania 3: %i\n", ReadTime_1(FindChar_3));
+	}
+	printf("\n");
+	for(int i=0; i<4; i++){
+	printf("Czas przetwarzania 4: %i\n", ReadTime_1(FindChar_4));
+	}
+	printf("\n");
+	for(int i=0; i<4; i++){
+	printf("Czas przetwarzania 5: %i\n", ReadTime_1(FindChar_5));
+	}
+	printf("\n");
+	for(int i=0; i<4; i++){
+	printf("Czas przetwarzania 6: %i\n", ReadTime_1(FindChar_6));
 	}
 
-	FreeLibrary(DynamicLib);
+
+
+
+	MessageBox (NULL, L"Wyci¹gnij wnioski", L"Lab. 2", MB_OK);
+
+
 	
 	return 0; 
 }
--- JAApp/JADII/mojadll.asm
+++ JAApp/JADII/mojadll.asm
@@ -1,69 +1,240 @@
 ;-------------------------------------------------------------------------
- .386 
- .MODEL FLAT, STDCALL
+.386 
+.MODEL FLAT, STDCALL
+
 OPTION CASEMAP:NONE
-INCLUDE C:\masm32\include\windows.inc
-.data
-msg db 'Hello, World!', 0Dh, 0Ah,'$'
-len equ $ - msg
-.CODE
+
+.NOLIST
+.NOCREF
+INCLUDE    \masm32\include\windows.inc 
+.LIST
+
+.data 
+DataString DB 'AGIKSZJ', 0FFH	; definicja ciagu znakow  
+
+.code 
+
 DllEntry PROC hInstDLL:HINSTANCE, reason:DWORD, reserved1:DWORD
-mov eax, TRUE 
-ret
+
+    MOV EAX, TRUE 
+    RET
+
 DllEntry ENDP
-;------------------------------------------------------------------------- 
-; To jest przyk³adowa funkcja. Jest tutaj, aby pokazaæ, 
-; gdzie nale¿y umieszczaæ w³asne funkcje w bibliotece DLL 
-;-------------------------------------------------------------------------
-MyProc1 proc x: DWORD, y: DWORD
-xor eax,eax
-mov eax,x 
-mov ecx,y 
-ror ecx,1 
-shld eax,ecx,2 
-jnc ET1 
-mul y 
-ret 
-ET1: 
-Mul x 
-Neg y 
-ret
-MyProc1 endp
 
-MyProc2 proc x: DWORD, y:DWORD
+;**************************************************************************** 
+;* Procedura FindChar_1 wyszukiwania znaku 'J' w ciagu 'DataString' * 
+;* * 
+;* Bezposrednia adresacja indeksowa * 
+;* Parametry wejsciowe: * 
+;*	AH - szukany znak 'J' * 
+;* Parametry wyjsciowe: * 
+;*	EAX - BOOL TRUE Found, FALSE not found * 
+;* * 
+;**************************************************************************** 
+FindChar_1 PROC 
+  
+    MOV ESI, OFFSET DataString	; zaladuj offset zmiennej 'DataString' do rej. ESI 
+    MOV AL, 'J'					; zaladuj kod litery 'J' do rej. AH 
+Check_End: 
+    CMP BYTE PTR [ESI], 0FFH	; czy koniec lancucha (znak specjalny FF)? 
+    JE Not_Find					; znaleziono znak konca (wartownik) 
+    CMP AH, [ESI]			; porownaj znak z elementem lancucha 'DataString' 
+    JE Got_Equal				; znaleziono znak! 
+    ADD ESI, 1					; inkrementuj offset
+    JMP Check_End				; petla wyszukiwania 
+Got_Equal: 
+    MOV DL, [ESI]				; zaladuj znaleziony znak do DL 
+    JMP Done 
+Not_Find: 
+    MOV EAX,0					; nie znaleziono znaku 
+    RET							; powrot z procedury 
+Done: 
+    MOV EAX,1					; znaleziono znak 
+    RET					; powrot z procedury 
+FindChar_1 ENDP					; koniec FindChar_1 
 
-;mov dx, msg
-;mov cx, len
-;mov bx, 1
-;mov ah, 40h
-;int 21h
- 
-std
-cld
-sub eax, eax
+;**************************************************************************** 
+;* Procedura FindChar_2 wyszukiwania znaku 'J' w ciagu 'LocalString' * 
+;* * 
+;* Bezposrednia adresacja indeksowa * 
+;* Parametry wejsciowe: * 
+;*	AH - szukany znak 'J' * 
+;* Parametry wyjsciowe: * 
+;*	EAX - BOOL TRUE Found, FALSE not found * 
+;* * 
+;**************************************************************************** 
+LocalString DB 0C3H,'GIKSZJ', 0FFH ; definicja ciagu znakow 
+FindChar_2 PROC 
+    MOV ESI, OFFSET LocalString ; zaladuj offset zmiennej 'LocalString' do rej. ESI 
+    MOV AH, 'J'					; zaladuj kod litery 'J' do rej. AH 
+Check_End: 
+    CMP BYTE PTR [ESI], 0FFH	; czy koniec lancucha (znak specjalny FF)? 
+    JE Not_Find				; znaleziono znak konca (wartownik) 
+    CMP AH, [ESI]			; porownaj znak z elementem lancucha 'LocalString' 
+    JE Got_Equal				; znaleziono znak! 
+    ADD ESI, 1					; inkrementuj offset 
+    JMP Check_End				; petla wyszukiwania 
+Got_Equal: 
+    MOV DL, [ESI]				; zaladuj znaleziony znak do DL 
+    JMP Done 
+Not_Find: 
+    MOV EAX,0					; nie znaleziono znaku 
+    RET							; powrot z procedury 
+Done: 
+    MOV EAX,1					; znaleziono znak 
+    RET							; powrot z procedury 
+FindChar_2 ENDP					; koniec FindChar_2 
+;**************************************************************************** 
+;* Procedura FindChar_3 wyszukiwania znaku 'J' w ciagu 'AppString' * 
+;* * 
+;* Bezposrednia adresacja indeksowa * 
+;* Parametry wejsciowe: * 
+;* AH - szukany znak 'J' * 
+;* Rej: ESI - offset adresu zmiennej 'AppString' * 
+;* Parametry wyjsciowe: * 
+;* EAX - BOOL TRUE Found, FALSE not found * 
+;* * 
+;**************************************************************************** 
+FindChar_3 PROC AppString: DWORD 
+    MOV ESI, AppString		; zaladuj offset zmiennej 'AppString' do rej. ESI 
+    MOV AH, 'J'					; zaladuj kod litery 'J' do rej. AH 
+Check_End: 
+    CMP BYTE PTR [ESI], 0FFH	; czy koniec lancucha (znak specjalny FF)? 
+    JE Not_Find					; znaleziono znak konca (wartownik) 
+    CMP AH, [ESI]			; porownaj znak z elementem lancucha 'AppString' 
+    JE Got_Equal				; znaleziono znak! 
+    ADD ESI, 1					; inkrementuj offset 
+    JMP Check_End				; petla wyszukiwania 
+Got_Equal: 
+    MOV DL, [ESI]				; zaladuj znaleziony znak do DL 
+    JMP Done 
+Not_Find: 
+    MOV EAX,0					; nie znaleziono znaku 
+    RET							; powrot z procedury 
+Done: 
+    MOV EAX,1
+	RET					; znaleziono znak 
+FindChar_3 ENDP					; koniec FindChar_3 
+;**************************************************************************** 
+;* Procedura FindChar_4 wyszukiwania znaku 'J' w ciagu 'DataString' * 
+;* * 
+;* Bezposrednia adresacja indeksowa * 
+;* Parametry wejsciowe: * 
+;* Rej: ESI - indeks zmiennej 'DataString' * 
+;* AH - szukany znak 'J' * 
+;* Parametry wyjsciowe: * 
+;* EAX - BOOL TRUE Found, FALSE not found * 
+;* * 
+;**************************************************************************** 
+FindChar_4 PROC NEAR			; deklaracja procedury FindChar_4 
+     MOV ESI, OFFSET DataString
+Check_End:
+	;CMP BYTE PTR [ESI], 0FFH
+	MOV AH, BYTE PTR [ESI]
+	SUB AH, 0FFH
+	JE Not_Find
+	MOV AH, [ESI]
+	SUB AH, 'J'
+	JE Got_Equal
+	ADD ESI, 1
+	JMP Check_End
+Got_Equal:
+	MOV EAX,1
+	RET
+Not_Find:
+	RET
 
-sub eax, y
-add eax, y
-mov eax, x
-add eax, y
+									; powrot z procedury 
+FindChar_4 ENDP					; koniec FindChar_4 
+;**************************************************************************** 
+;* Procedura FindChar_5 wyszukiwania znaku 'J' w ciagu 'DataString' * 
+;* * 
+;* Adresacja Base + Index * 
+;* Parametry wejsciowe: * 
+;* Rej: BX - offset zmiennej 'DataString' * 
+;* Parametry wyjsciowe: * 
+;* EAX - BOOL TRUE Found, FALSE not found * 
+;* * 
+;**************************************************************************** 
+FindChar_5 PROC NEAR 
+    MOV EBX, OFFSET DataString	; zaladuj offset zmiennej 'DataString' do rej. EBX 
+    CMP BYTE PTR [EBX+0], 'J'	; porownaj znak z elementem lancucha 'DataString' 
+    JE Got_It					; znaleziono znak 
+    CMP BYTE PTR [EBX+1], 'J'	; porownaj znak z elementem lancucha 'DataString' 
+    JE Got_It					; znaleziono znak 
+    CMP BYTE PTR [EBX+2], 'J'	; porownaj znak z elementem lancucha 'DataString' 
+    JE Got_It					; znaleziono znak 
+    CMP BYTE PTR [EBX+3], 'J'	; porownaj znak z elementem lancucha 'DataString' 
+    JE Got_It					; znaleziono znak 
+    CMP BYTE PTR [EBX+4], 'J'	; porownaj znak z elementem lancucha 'DataString' 
+    JE Got_It					; znaleziono znak 
+    CMP BYTE PTR [EBX+5], 'J'	; porownaj znak z elementem lancucha 'DataString' 
+	JE Got_It					; znaleziono znak 
+	CMP BYTE PTR [EBX+6], 'J'	; porownaj znak z elementem lancucha 'DataString' 
+	JE Got_It
+Not_Find: 
+    MOV EAX,0					; zaladuj znak zapytania do DL 
+    RET							; powrot z procedury 
+Got_It: 
+    MOV EAX,1					; wyswietl znak ne ekranie 
+    RET							; powrot z procedury 
+FindChar_5 ENDP					; koniec FindChar_5 
+;**************************************************************************** 
+;* Procedura FindChar_6 wyszukiwania znaku 'J' w ciagu 'DataString' * 
+;* * 
+;* adresacja Disp [EBX+ESI] * 
+;* Parametry wejsciowe: * 
+;* Rej: EBX - offset zmiennej 'DataString' * 
+;* ESI - przemieszczenie * 
+;* AH - szukany znak 'J' * 
+;* Parametry wyjsciowe: * 
+;* EAX - BOOL TRUE Found, FALSE not found * 
+;* * 
+;**************************************************************************** 
+FindChar_6 PROC NEAR 
+    MOV EBX, OFFSET DataString	; zaladuj offset zmiennej 'String' do rej. ESI 
+    XOR ESI, ESI				; wyzeruj indeks lancucha 'String' w ESI 
+    MOV AH, 'J'					; zaladuj kod litery 'J' do rej. AH 
+Check_End: 
+    CMP BYTE PTR [EBX+ESI], 0FFH; czy koniec lancucha (znak specjalny FF)? 
+    JE Not_Find					; znaleziono znak konca (wartownik) 
+    CMP AH, BYTE PTR [EBX+ESI]	; porownaj znak z elementem lancucha 'String' 
+    JE Got_Equal					; znaleziono znak! 
+    INC ESI						; inkrementuj indeks 
+    JMP Check_End				; petla wyszukiwania 
+Got_Equal: 
+    MOV DL, [EBX+ESI]			; zaladuj znaleziony znak do DL 
+    JMP Done 
+Not_Find: 
+    MOV EAX,0					; nie znaleziono znaku 
+    RET 
+Done: 
+    MOV EAX,1					; znaleziono znak 
+    RET							; powrot z procedury 
+FindChar_6 ENDP					; koniec FindChar_6 
+;****************************************************************************
+;* Procedura ReadTime_1 pomiaru czasu wykonania procedury FindChar_1 * 
+;* * 
+;* do pomiaru wykorzystywany jest licznik taktów zegara procesora * 
+;* Parametry wejsciowe: * 
+;* Parametry wyjsciowe: * 
+;* EAX - czas w taktach zegara * 
+;* * 
+;**************************************************************************** 
+.586
+ReadTime_1 PROC fnptr: DWORD			;PROC a1: DWORD, a2: DWORD, a3: DWORD, a4:DWORD, a5:DWORD 
+    CPUID						; celowo brak komentarza
+
+    RDTSC						; odczyt licznika taktów
+    MOV ECX,EAX
+	
+    CALL fnptr				; wywo³anie badanej funkcji
+
+    RDTSC
+    SUB EAX,ECX
 
 
-ret
-
-MyProc2 endp
-
-
-END DllEntry
-;-------------------------------------------------------------------------
-
-
-
-
-
-
-;std
-;cld
-;xor eax, eax
-;sub eax, y
-;mov eax, x
-;add eax, y
+    RET
+ReadTime_1 ENDP 
+;**************************************************************************** 
+END DllEntry 
