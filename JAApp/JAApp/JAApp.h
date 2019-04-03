34ecf3b9500eb5f52110d188d5facacd1c10278f
bartoszskrobacki <bartoszskrobacki@gmail.com>
2019-04-03 19:45:20 +0200

    2

---
+++ JAApp/JAApp/JAApp.h
@@ -0,0 +1,8 @@
+#include <windows.h>
+extern "C" int _stdcall FindChar_1 (void);
+extern "C" int _stdcall FindChar_2 (void);
+extern "C" int _stdcall FindChar_3 (LPSTR String);
+extern "C" int _stdcall FindChar_4 (void);
+extern "C" int _stdcall FindChar_5 (void);
+extern "C" int _stdcall FindChar_6 (void);
+extern "C" int _stdcall ReadTime_1 (void* fnptr);
